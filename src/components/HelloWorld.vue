a5bf20813cdf302fa8dbf1534008e99ef0972ff2
fangbo <649392395@qq.com>
2021-12-30 15:34:42 +0800

    新建

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="">
+  <van-cell-group>
+    <van-cell title="单元格" :value="msg" />
+  </van-cell-group>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
